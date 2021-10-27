226b4c8e68f841e8509520f581c0fe3ca952c761
songthan9xa1 <76012525+songthan9xa1@users.noreply.github.com>
2021-10-28 01:08:12 +0200

    Update card.php

--- card.php
+++ card.php
@@ -1,7 +1,10 @@
 <div class="card">
+  <div class="card-title"><?=$row['titel']?></div>
   <img src="https://image.yes24.vn/Upload/ProductImage/thethaochinhang/2063162_L.jpg?width=550&height=550" class="card-img-top" alt="products">
   <div class="card-body">
-    test
+    <?=$row['beschreibung']?>
+    <hr>
+    <?=$row['price']?>
   </div>
   <div class="card-footer">
     <a href="" class="btn btn-primary btn-sm">details</a>
