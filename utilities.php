e94b7e8e460fa46fdcf6709251146ffe3ea78148
songthan9xa1 <76012525+songthan9xa1@users.noreply.github.com>
2021-11-05 15:39:32 +0100

    Delete utilities.php

--- utilities.php
+++
@@ -1,9 +0,0 @@
-<?php
-function isPost():bool
-{
-  return strtoupper($_SERVER['REQUEST_METHOD']) === 'POST';
-}
-function escapse(string $value):string
-{
-  return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
-}
