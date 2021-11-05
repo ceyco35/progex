594e1ce1d1fd7d38a85f606766b58f69fd73a553
songthan9xa1 <76012525+songthan9xa1@users.noreply.github.com>
2021-11-05 15:35:33 +0100

    Create paypal.php

---
+++ config/paypal.php
@@ -0,0 +1,5 @@
+<?php
+define('PAYPAL_CLIENT_ID','AdXJeMzcIDJ7TVgGc6scyyYNSm1mKwSDu3qjOcGNUblWiSJw4xg1QzVUCNbSb-ONgHWI4h-fNEDEXkCO');
+define('PAYPAL_SECRET','EA9twn5_EgqvnOkFkbYZuoeR8OVC0adB_Pfq69y8Z4FHSOMmJlh1XxbPjHmWupe0VpZr--DybkLYnf45');
+define('PAYPAL_BASE_URL','https://api.sandbox.paypal.com');
+ ?>
