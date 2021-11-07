3aac79cfb028bd37a6b7a23de5c47f0b566718ba
Ceylan <yildirim.ceylan@outlook.com>
2021-11-07 23:24:13 +0100

    changed

--- products.sql
+++ products.sql
@@ -2,8 +2,8 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Erstellungszeit: 05. Nov 2021 um 15:50
+-- Host: localhost
+-- Erstellungszeit: 07. Nov 2021 um 23:22
 -- Server-Version: 10.4.21-MariaDB
 -- PHP-Version: 8.0.12
 
@@ -40,10 +40,13 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `titel`, `describtion`, `price`, `image`) VALUES
-(1, 'Test product 1', 'Beschreibung 1', 4.99, 'https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/b1f0f0de-35a3-4078-9dc4-27ea8e69385a/air-force-1-next-nature-brown-kelp-dc8744-301-%E2%80%93-erscheinungsdatum.jpg'),
-(2, 'Test product 2', 'Beschreibung 2', 5.99, 'https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/016fe58c-cff9-42da-8545-1606cf326c10/custom-nike-air-force-1-low-cozi-custom-shoe.png'),
-(3, 'Test product 3', 'Beschreibung 3', 9.99, 'https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/d9c61530-88e8-46c2-a3aa-fef1d49de7f7/air-force-1-experimental-herrenschuh-6VmxCl.png'),
-(4, 'Test product 4', 'Beschreibung 4', 10.56, 'https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/c3916202-2131-461b-8dd2-f6daebdea302/pegasus-trail-3-trail-laufschuh-fur-3BMMJx.png');
+(7, 'Nike Air Max 97', 'Herrenschuh', 197.99, 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/1632439a-15ca-4213-842e-4e8b062a6c86/air-max-97-herrenschuh-CV3dl7.png'),
+(1, 'Airforce', 'Damenschuh', 89.9, 'https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/b1f0f0de-35a3-4078-9dc4-27ea8e69385a/air-force-1-next-nature-brown-kelp-dc8744-301-%E2%80%93-erscheinungsdatum.jpg'),
+(2, 'Airforce', 'Damenschuh', 89.9, 'https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/016fe58c-cff9-42da-8545-1606cf326c10/custom-nike-air-force-1-low-cozi-custom-shoe.png'),
+(3, 'Airforce', 'Damenschuh', 89.9, 'https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/d9c61530-88e8-46c2-a3aa-fef1d49de7f7/air-force-1-experimental-herrenschuh-6VmxCl.png'),
+(4, 'Nike Pegasus Trail 3', 'Damenschuh', 94.9, 'https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/c3916202-2131-461b-8dd2-f6daebdea302/pegasus-trail-3-trail-laufschuh-fur-3BMMJx.png'),
+(5, 'Air VaporMax 2021', 'Damenschuh', 140, 'https://www.snipes.com/dw/image/v2/BDCB_PRD/on/demandware.static/-/Sites-snse-master-eu/default/dwd8d5eaad/1929674_P.jpg?sw=780&sh=780&sm=fit&sfrm=png'),
+(6, 'Air VaporMax 2021 FK', 'Damenschuh', 140, 'https://www.snipes.com/dw/image/v2/BDCB_PRD/on/demandware.static/-/Sites-snse-master-eu/default/dw8b970589/1929690_P.jpg?sw=780&sh=780&sm=fit&sfrm=png');
 
 --
 -- Indizes der exportierten Tabellen
@@ -63,7 +66,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT für Tabelle `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
